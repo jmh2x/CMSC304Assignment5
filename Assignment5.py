3c92e5ecb9751bf6ff078bf1527d97cba7b1fe23
GovanHenry <ghenry3217@gmail.com>
2025-03-16 17:31:17 -0400

    customCanvas

--- Assignment5.py
+++ Assignment5.py
@@ -1,5 +1,10 @@
-#Class CustomCanvas
+import tkinter as tk
 
+
+#Class CustomCanvas
+class CustomCanvas:
+     def __init__(self, height: int, width: int):
+        self.canvas = tk.Canvas(height=height, width=width)
 
 #Class Rectangle
 
