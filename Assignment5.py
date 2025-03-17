894bc20002354297bc92055470df87b68f6b235e
GovanHenry <ghenry3217@gmail.com>
2025-03-16 20:35:36 -0400

    rectangle

--- Assignment5.py
+++ Assignment5.py
@@ -1,15 +1,19 @@
-import tkinter as tk
+import tkinter as tkin
 
 
 #Class CustomCanvas
 class CustomCanvas:
      def __init__(self, height: int, width: int):
-        self.canvas = tk.Canvas(height=height, width=width)
+        self.canvas = tkin.Canvas(height=height, width=width)
 
 #Class Rectangle
-
+class Rectangle:
+    def __init__(self, x: int, y: int, width: int, height: int):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
 
 #def pack(allRect, canvasSize)
-
 
 #def main()
